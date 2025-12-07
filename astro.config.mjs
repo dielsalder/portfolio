4457fafcf0ec6aa5665c6e707d433c066e626b4c
Jude He <>
2025-12-07 16:45:05 -0500

    add fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,5 +11,44 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()]
+  },
+  experimental: {
+    fonts: [{
+      provider: "local",
+      name: "Futura",
+      cssVariable: "--font-futura",
+      variants: [
+        {
+          weight: 300,
+          style: "normal",
+          src: ["./src/assets/fonts/FuturaPTLight.otf"]
+        },
+        {
+          weight: 400,
+          style: "normal",
+          src: ["./src/assets/fonts/FuturaPTBook.otf"]
+        },
+        {
+          weight: 500,
+          style: "normal",
+          src: ["./src/assets/fonts/FuturaPTMedium.otf"]
+        },
+        {
+          weight: 600,
+          style: "normal",
+          src: ["./src/assets/fonts/FuturaPTHeavy.otf"]
+        },
+        {
+          weight: 700,
+          style: 'normal',
+          src: ["./src/assets/fonts/FuturaPTBold.otf"]
+        },
+        {
+          weight: 700,
+          style: 'normal',
+          src: ["./src/assets/fonts/FuturaPTExtraBold.otf"]
+        }
+      ]
+    }]
   }
 });
